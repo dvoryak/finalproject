9a52f3f01fae8aa46d3f1ad6dd05679e7c160d04
paveldvoryak <p380970610647@gmail.com>
2018-01-01 23:03:34 +0200

    some fixes

---
+++ src/main/java/dao/AbstractDAO.java
@@ -0,0 +1,38 @@
+package dao;
+
+import model.entity.Entity;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public abstract class AbstractDAO<K, E extends Entity> implements BasicCrudOperation<K,E> {
+
+    private Connection connection;
+
+    public AbstractDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void closeStatement(Statement statement) {
+        try {
+
+            if (statement != null) {
+                statement.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // TODO add logger here
+        }
+    }
+
+    public void closeConnection(Connection connection) {
+        try {
+            if(connection != null)
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // TODO add logger here
+        }
+    }
+}
