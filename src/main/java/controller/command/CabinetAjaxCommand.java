4254d7df69da93ffbea44097763b547cca61424a
paveldvoryak <p380970610647@gmail.com>
2018-01-23 11:27:24 +0200

    remarks correction

--- src/main/java/controller/command/CabinetAjaxCommand.java
+++ src/main/java/controller/command/CabinetAjaxCommand.java
@@ -29,33 +29,51 @@ public class CabinetAjaxCommand implements Command {
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
         User user = (User) request.getSession().getAttribute("user");
+        String status = request.getParameter("status");
+        String resp = null;
+
         if (user.getRole().equals("Inspector")) {
             reports = reportService.findByInspectorId(user.getId());
+
         } else if (user.getRole().equals("Client")) {
             reports = reportService.findByUserId(user.getId());
         }
-        String name = request.getParameter("status");
-        String resp = null;
-        if (name != null) {
-            resp = getJSON(reports.stream()
-                    .filter(report -> report.getStatus().equals(Report.Status.valueOf(name.toUpperCase())))
-                    .sorted(Comparator.comparingLong(o -> o.getDate().getTime()))
-                    .collect(Collectors.toList()));
+
+
+        if (status != null) {
             try {
-                response.getWriter().append(resp);
+                response.getWriter()
+                        .append(formResult(status));
+
             } catch (IOException e) {
                 e.printStackTrace();
                 throw e;
             }
-
         }
 
         return null;
     }
 
+
     private String getJSON(List<Report> reports) {
         Gson gson = new Gson();
         return gson.toJson(reports);
+    }
+
+
+    private String formResult(String status) {
+
+        if(status.equals("checked")) {
+            return getJSON(reports.stream()
+                    .filter(report -> report.getStatus() != Report.Status.UNCHECKED)
+                    .sorted(Comparator.comparingLong(o -> o.getDate().getTime()))
+                    .collect(Collectors.toList()));
+        }
+
+        return getJSON(reports.stream()
+                .filter(report -> report.getStatus().equals(Report.Status.valueOf(status.toUpperCase())))
+                .sorted(Comparator.comparingLong(o -> o.getDate().getTime()))
+                .collect(Collectors.toList()));
     }
 
 }
